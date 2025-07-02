1c164040042271d53369251ca97aba3b43b166ff
dshree-34 <dhanushreesenthilkumar2811@gmail.com>
2025-07-02 15:34:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";  
+import MovieFeedbackForm from "./components/MovieFeedbackForm";
+
+
+function App() {
+  return (
+    <div className="App">
+      <MovieFeedbackForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MovieFeedbackForm.js
@@ -0,0 +1,77 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
+
+const MovieFeedbackForm = () => {
+  const initialValues = {
+    Name: "",
+    MovieName: "",
+    ratings: "",
+    comments: "",
+    
+  };
+
+  const validationSchema = Yup.object({
+    Name: Yup.string()
+      .min(3, "Name must be at least 3 characters")
+      .required("Full name is required"),
+
+    MovieName: Yup.string()
+      .required("MovieName is required"),
+
+    ratings: Yup.number()
+      .min(1, "Minimum rating is 1")
+      .max(5, "Maximum rating is 5")
+      .required("Rating is required"),
+
+    comments: Yup.string()
+      .max(50, "comments should be under 50 characters")
+  });
+  const handleSubmit = (values, { resetForm }) => {
+    console.log("Form submitted: ", values);
+    alert("Form submitted successfully");
+    resetForm();
+  };
+
+  return (
+    <div className="form-container">
+      <h2>MOVIE FEEDBACK FORM</h2>
+
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div>
+            <label>Name:</label>
+            <Field type="text" name="Name" />
+            <ErrorMessage name="Name" component="div" className="error" />
+          </div>
+
+          <div>
+            <label>MovieName:</label>
+            <Field type="MovieName" name="MovieName" />
+            <ErrorMessage name="MovieName" component="div" className="error" />
+          </div>
+
+          <div>
+            <label>ratings (1-5):</label>
+            <Field type="number" name="ratings" min="1" max="5" />
+            <ErrorMessage name="ratings" component="div" className="error" />
+          </div>
+
+          <div>
+            <label>comments:</label>
+            <Field as="textarea" name="comments" />
+            <ErrorMessage name="comments" component="div" className="error" />
+          </div>
+
+          <button type="submit">Submit Feedback</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default MovieFeedbackForm;
+
